fe1c375e3d9ee8131b7b5442119f8ac40f6a4007
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-06-17 23:20:08 +0200

    Add CLI parser

--- source/app.d
+++ source/app.d
@@ -5,8 +5,69 @@ Author: Joakim Brännström (joakim.brannstrom@gmx.com)
 */
 module app;
 
-import std.stdio;
+import std.algorithm : among;
+
+import logger = std.experimental.logger;
 
 int main(string[] args) {
-    return 0;
+    Config conf;
+
+    parseCLI(args, conf);
+
+    final switch (conf.mode) {
+    case AppMode.none:
+        return 0;
+    case AppMode.help:
+        return 0;
+    case AppMode.helpUnknownCommand:
+        return 1;
+    }
+}
+
+enum AppMode {
+    none,
+    help,
+    helpUnknownCommand,
+}
+
+struct Config {
+    AppMode mode;
+}
+
+void parseCLI(string[] args, ref Config conf) {
+    static import std.getopt;
+
+    int dummy;
+    std.getopt.GetoptResult help_info;
+    try {
+        // dfmt off
+        help_info = std.getopt.getopt(args,
+            std.getopt.config.keepEndOfOptions,
+            "x", "dummy", &dummy,
+            );
+        // dfmt on
+        conf.mode = help_info.helpWanted ? AppMode.help : AppMode.none;
+    } catch (std.getopt.GetOptException e) {
+        // unknown option
+        logger.error(e.msg);
+        conf.mode = AppMode.helpUnknownCommand;
+    } catch (Exception e) {
+        logger.error(e.msg);
+        conf.mode = AppMode.helpUnknownCommand;
+    }
+
+    logger.trace(conf);
+
+    void printHelp() {
+        import std.getopt : defaultGetoptPrinter;
+        import std.format : format;
+        import std.path : baseName;
+
+        defaultGetoptPrinter(format("usage: %s\n", args[0].baseName), help_info.options);
+    }
+
+    if (conf.mode.among(AppMode.help, AppMode.helpUnknownCommand)) {
+        printHelp;
+        return;
+    }
 }
