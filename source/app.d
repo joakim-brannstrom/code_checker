a6e02b59ef4db6c01b0ddf398416af86151129a4
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-06-18 22:37:52 +0200

    Find all compilation databases

--- source/app.d
+++ source/app.d
@@ -6,8 +6,10 @@ Author: Joakim Brännström (joakim.brannstrom@gmx.com)
 module app;
 
 import std.algorithm : among;
+import std.exception : collectException;
 
 import logger = std.experimental.logger;
+import code_checker.types : AbsolutePath, Path;
 
 int main(string[] args) {
     import std.functional : toDelegate;
@@ -23,13 +25,13 @@ int main(string[] args) {
     cmds[AppMode.none] = toDelegate(&modeNone);
     cmds[AppMode.help] = toDelegate(&modeNone);
     cmds[AppMode.helpUnknownCommand] = toDelegate(&modeNone_Error);
+    cmds[AppMode.normal] = toDelegate(&modeNormal);
 
     if (auto v = conf.mode in cmds) {
         return (*v)(conf);
     }
 
     logger.error("Unknown mode %s", conf.mode);
-
     return 1;
 }
 
@@ -41,10 +43,53 @@ int modeNone_Error(const ref Config conf) {
     return 1;
 }
 
+int modeNormal(const ref Config conf) {
+    auto compile_dbs = findCompileDbs(conf.compileDbs);
+
+    return 0;
+}
+
+auto findCompileDbs(const(AbsolutePath)[] paths) {
+    import std.algorithm : filter, map;
+    import std.file : exists, isDir, isFile, dirEntries, SpanMode;
+
+    AbsolutePath[] rval;
+
+    static AbsolutePath[] findRecursive(const AbsolutePath p) {
+        import std.path : baseName;
+
+        AbsolutePath[] rval;
+        foreach (a; dirEntries(p, SpanMode.depth).filter!(a => a.isFile)
+                .filter!(a => a.name.baseName == "compile_commands.json").map!(a => a.name)) {
+            try {
+                rval ~= AbsolutePath(Path(a));
+            } catch (Exception e) {
+                logger.warning(e.msg);
+            }
+        }
+        return rval;
+    }
+
+    foreach (a; paths.filter!(a => exists(a))) {
+        try {
+            if (a.isDir) {
+                logger.tracef("Looking for compilation database in '%s'", a).collectException;
+                rval ~= findRecursive(a);
+            } else if (a.isFile)
+                rval ~= a;
+        } catch (Exception e) {
+            logger.warning(e.msg);
+        }
+    }
+
+    return rval;
+}
+
 enum AppMode {
     none,
     help,
     helpUnknownCommand,
+    normal,
 }
 
 struct Config {
@@ -52,9 +97,14 @@ struct Config {
 
     AppMode mode;
     VerboseMode verbose;
+
+    /// Either a path to a compilation database or a directory to search for one in.
+    AbsolutePath[] compileDbs;
 }
 
 void parseCLI(string[] args, ref Config conf) {
+    import std.algorithm : map;
+    import std.array : array;
     import code_checker.logger : VerboseMode;
     static import std.getopt;
 
@@ -62,14 +112,16 @@ void parseCLI(string[] args, ref Config conf) {
     bool verbose_trace;
     std.getopt.GetoptResult help_info;
     try {
+        string[] compile_dbs;
         // dfmt off
         help_info = std.getopt.getopt(args,
             std.getopt.config.keepEndOfOptions,
+            "c|compile-db", "path to a compilationi database or where to search for one", &compile_dbs,
             "v|verbose", "verbose mode is set to information", &verbose_info,
             "vverbose", "verbose mode is set to trace", &verbose_trace,
             );
         // dfmt on
-        conf.mode = help_info.helpWanted ? AppMode.help : AppMode.none;
+        conf.mode = help_info.helpWanted ? AppMode.help : AppMode.normal;
         conf.verbose = () {
             if (verbose_trace)
                 return VerboseMode.trace;
@@ -77,6 +129,9 @@ void parseCLI(string[] args, ref Config conf) {
                 return VerboseMode.info;
             return VerboseMode.minimal;
         }();
+        conf.compileDbs = compile_dbs.map!(a => Path(a).AbsolutePath).array;
+        if (compile_dbs.length == 0)
+            conf.compileDbs = [AbsolutePath(Path("."))];
     } catch (std.getopt.GetOptException e) {
         // unknown option
         logger.error(e.msg);
